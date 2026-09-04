976e2cc11cf927ce53e986b3c5b01a0f317d4f19
Fu Lim <fulim1130@gmail.com>
2026-09-05 06:48:53 +0800

    Building

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"expvar"
 	"flag"
+	"fmt"
 	"log/slog"
 	"os"
 	"runtime"
@@ -14,10 +15,13 @@ import (
 
 	"github.com/Fulim13/movie-api/internal/data"
 	"github.com/Fulim13/movie-api/internal/mailer"
+	"github.com/Fulim13/movie-api/internal/vcs"
 	_ "github.com/lib/pq"
 )
 
-const version = "1.0.0"
+var (
+	version = vcs.Version()
+)
 
 type config struct {
 	port int
@@ -58,7 +62,7 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "Server Port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
-	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://greenlight:pa55word@localhost/greenlight?sslmode=disable", "Postgres DSN")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "", "Postgres DSN")
 	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
 	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
 	flag.DurationVar(&cfg.db.maxIdleTime, "db-max-idle-time", 15*time.Minute, "PostgreSQL max connection idle time")
@@ -74,8 +78,13 @@ func main() {
 		cfg.cors.trustedOrigins = strings.Fields(s)
 		return nil
 	})
+	displayVerion := flag.Bool("version", false, "Display version and exit")
 
 	flag.Parse()
+	if *displayVerion {
+		fmt.Printf("Version: \t%s\n", version)
+		os.Exit(0)
+	}
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
