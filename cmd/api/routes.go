e44750dc4140d9ee8db5e5ca45f2cfe8dfba80c1
Fu Lim <fulim1130@gmail.com>
2026-09-05 05:33:49 +0800

    Custom Metric

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -25,5 +25,5 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
 
 	router.Handler(http.MethodGet, "/debug/var", expvar.Handler())
-	return app.recoverPanic(app.enableCORS(app.rateLimit(app.authenticate(router))))
+	return app.metrics(app.recoverPanic(app.enableCORS(app.rateLimit(app.authenticate(router)))))
 }
